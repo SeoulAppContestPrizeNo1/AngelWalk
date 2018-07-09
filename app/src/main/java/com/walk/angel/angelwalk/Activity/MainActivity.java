eefd25234ae4914a6b29eb4907f6daa59fb3d100
hanareum95 <areum9502@gmail.com>
2018-07-09 21:26:33 +0900

    시작화면 (Home fragment) 지정

--- app/src/main/java/com/walk/angel/angelwalk/Activity/MainActivity.java
+++ app/src/main/java/com/walk/angel/angelwalk/Activity/MainActivity.java
@@ -35,24 +35,7 @@ public class MainActivity extends AppCompatActivity {
         MainTabPagerAdapter pagerAdapter = new MainTabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        // Set TabSelectedListener
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        viewPager.setCurrentItem(1);
 
     }
 }
