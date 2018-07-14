37a6cb78661cbafc705e6f99fbde80e3f1b4e0c0
ahyeonway <ahyeoncode@gmail.com>
2018-07-15 06:21:25 +0900

    SightsFragment작성

---
+++ app/src/main/java/com/walk/angel/angelwalk/Fragment/SightsFragment.java
@@ -0,0 +1,9 @@
+package com.walk.angel.angelwalk.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SightsFragment extends Fragment {
+
+}
